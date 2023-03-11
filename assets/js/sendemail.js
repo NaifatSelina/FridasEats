e6b0aade37cc911505d91006c8fa1d3a8b71cc27
NaifatSelina <naifatselina@icloud.com>
2023-03-11 23:22:55 +0000

    Try to add js to link email for newsletter form

---
+++ assets/js/sendemail.js
@@ -0,0 +1,16 @@
+function sendEmail(contactForm) {
+    email.js.send("service_i73601k", "template_fgjkd8n", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false; 
+}
