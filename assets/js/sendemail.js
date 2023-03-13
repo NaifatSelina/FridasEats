1786dcc61dc6074a3054312f90830ea5d1299993
NaifatSelina <naifatselina@icloud.com>
2023-03-13 12:54:28 +0000

    Fix typo that stopped newsletter form from working and move script tag to head element

--- assets/js/sendemail.js
+++ assets/js/sendemail.js
@@ -1,15 +1,17 @@
 function sendEmail(contactForm) {
-    email.js.send("service_i73601k", "template_fgjkd8n", {
-        "from_name": contactForm.first_name.value,
-        "from_email": contactForm.emailaddress.value,
-    })
-    .then(
-        function(response) {
-            console.log("SUCCESS", response);
-        },
-        function(error) {
-            console.log("FAILED", error);
-        }
-    );
-    return false; 
+    event.preventDefault()
+    console.log("Sending an email... ")
+    emailjs.send("service_i73601k", "template_fgjkd8n", {
+            "from_name": contactForm.first_name.value + " " + contactForm.last_name.value,
+            "from_email": contactForm.emailaddress.value
+        })
+        .then(
+            function (response) {
+                console.log("SUCCESS", response);
+            },
+            function (error) {
+                console.log("FAILED", error);
+            }
+        );
+    return false; // To block from loading a new page
 }
