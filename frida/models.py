46cbb50da84b12ff08271fd8c0317fa5ac09e533
NaifatSelina <naifatselina@icloud.com>
2023-03-28 15:39:33 +0000

    connect index.html file, create review model class and migrate

--- frida/models.py
+++ frida/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class Review(models.Model):
+
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="reviews"
+    )
+    email = models.EmailField()
+    content = models.TextField()
+    approved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
--- fridaseats/urls.py
+++ fridaseats/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from frida.views import home_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_page, name='home_page'),
 ]
