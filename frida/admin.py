1810d08453bf88b20dc239e2965f13065f6ce745
NaifatSelina <naifatselina@icloud.com>
2023-03-28 17:30:32 +0000

    Add ability for admin to make a review and submit the review

--- frida/admin.py
+++ frida/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Review
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+
+    actions = ['approve_reviews']
+
+    def approve_reviews(self, request, queryset):
+        queryset.update(approved=True)
--- frida/models.py
+++ frida/models.py
@@ -1,18 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
+RATING_CHOICES = [(1, '1 star'),    (2, '2 stars'),    (3, '3 stars'),    (4, '4 stars'),    (5, '5 stars'),]
+
 
 class Review(models.Model):
-
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="reviews"
     )
     email = models.EmailField()
     content = models.TextField()
+    rating = models.IntegerField(choices=RATING_CHOICES, validators=[MinValueValidator(1), MaxValueValidator(5)], default=0)
     approved = models.BooleanField(default=False)
 
+    class Meta:
+        ordering = ["author"]
+
     def __str__(self):
-        return self.title
+        return f"This review was submitted by {self.author}."
