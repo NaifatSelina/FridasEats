f88455fb8835b467a7b8f6e1b2da8ffcf0d2f8b6
NaifatSelina <naifatselina@icloud.com>
2023-03-28 17:47:32 +0000

    Add templates to templates folder and move stylesheet to static file

--- frida/views.py
+++ frida/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Review
 
-# Create your views here.
+
+class ReviewList(generic.ListView):
+    model = Review
+    queryset = Post.objects.filter(status=1).order_by("-author")
+    template_name = "index.html"
+    paginate_by = 3
 
 
 def home_page(request):
